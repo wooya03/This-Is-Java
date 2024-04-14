598512e12c22598b8ab8d0e74e93e77dcb5bcff0
Wooya <wooyya03@naver.com>
2024-04-15 03:10:09 +0900

    .

---
+++ src/ch04/sec10/AdvancedForEx.java
@@ -0,0 +1,12 @@
+package ch04.sec10;
+
+public class AdvancedForEx {
+    public static void main(String[] args) {
+        int[] scores = {95,71,84,93,87};
+        int sum = 0;
+        for(int score : scores){
+            sum = sum + score;
+        }
+        System.out.println(sum);
+    }
+}
