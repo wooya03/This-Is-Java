870a743195e0855b0a8635f0c67043143cb87300
Wooya <wooyya03@naver.com>
2024-04-15 00:49:57 +0900

    .

---
+++ src/ch04/sec03/ReferenceVariableCompareEx.java
@@ -0,0 +1,16 @@
+package ch04.sec03;
+
+public class ReferenceVariableCompareEx {
+    public static void main(String[] args) {
+        int[] arr1;
+        int[] arr2;
+        int[] arr3;
+
+        arr1 = new int[]{1,2,3};
+        arr2 = new int[]{1,2,3};
+        arr3 = arr2;
+
+        System.out.println(arr1 == arr2); // 각자 객체를 생성함
+        System.out.println(arr2 == arr3); // 같은 객체를 참조하고 있음
+    }
+}
---
+++ src/ch04/sec05/EqualsEx.java
@@ -0,0 +1,22 @@
+package ch04.sec05;
+
+public class EqualsEx {
+    public static void main(String[] args) {
+        String str1 = "A";
+        String str2 = "A";
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+
+        System.out.println("==============");
+
+        String str3 = new String("A");
+        String str4 = new String("A");
+        System.out.println(str3 == str4);
+        System.out.println(str3.equals(str4));
+
+        System.out.println("==============");
+
+        String str5 = "";
+        System.out.println(str5.equals(""));
+    }
+}
