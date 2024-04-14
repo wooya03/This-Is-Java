37f554cfb51dd0cfe171e64ac9a6090fa9b52717
Wooya <wooyya03@naver.com>
2024-04-15 03:08:09 +0900

    .

---
+++ src/ch04/sec09/ArrayCopyByForEx.java
@@ -0,0 +1,15 @@
+package ch04.sec09;
+
+public class ArrayCopyByForEx {
+    public static void main(String[] args) {
+        int[] oldIntArray = {1,2,3};
+        int[] newIntArray = new int[5];
+        for(int i = 0; i < oldIntArray.length; i++){
+            newIntArray[i] = oldIntArray[i];
+        }
+
+        for(int i = 0; i < newIntArray.length; i++){
+            System.out.println(newIntArray[i]);
+        }
+    }
+}
