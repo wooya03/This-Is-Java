cfe61f9a50d79ce5cbb27ee58a86767eb72fe848
Wooya <wooyya03@naver.com>
2024-04-15 00:55:37 +0900

    .

---
+++ src/ch04/sec05/CharAtEx.java
@@ -0,0 +1,19 @@
+package ch04.sec05;
+
+public class CharAtEx {
+    public static void main(String[] args) {
+        String ssn = "030421-4******";
+        char gender = ssn.charAt(7);
+        switch(gender){
+            case '1':
+            case '3':
+                System.out.println("당신의 성별은 남성입니다.");
+                break;
+            case '2':
+            case '4':
+                System.out.println("당신의 성별은 여성입니다.");
+                break;
+        }
+
+    }
+}
---
+++ src/ch04/sec05/LengthEx.java
@@ -0,0 +1,10 @@
+package ch04.sec05;
+
+public class LengthEx {
+    public static void main(String[] args) {
+        String str = "안녕하세요";
+        for(int i = 0; i < str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
