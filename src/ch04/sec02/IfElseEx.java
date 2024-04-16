364c3e81a323ac1a5bb8a7b7081278180c1dd074
Wooya <wooyya03@naver.com>
2024-04-16 09:30:35 +0900

    .

---
+++ src/ch04/sec02/IfElseEx.java
@@ -0,0 +1,12 @@
+package ch04.sec02;
+
+public class IfElseEx {
+    public static void main(String[] args) {
+        int score = 85;
+        if(score >= 90){
+            System.out.println("등급은 A입니다");
+        } else {
+            System.out.println("등급은 B입니다.");
+        }
+    }
+}
