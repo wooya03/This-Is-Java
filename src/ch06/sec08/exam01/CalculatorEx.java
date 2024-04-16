36bade014a5cdd3652fe222f6cf6fb4622898a11
Wooya <wooya_0@naver.com>
2024-04-16 11:51:02 +0900

    .

---
+++ src/ch06/sec08/exam01/CalculatorEx.java
@@ -0,0 +1,15 @@
+package ch06.sec08.exam01;
+
+public class CalculatorEx {
+    public static void main(String[] args) {
+        Calculator myCalc = new Calculator();
+        myCalc.powerOn();
+        int result1 = myCalc.plus(5, 6);
+        System.out.println(result1);
+        int x = 10;
+        int y = 4;
+        double result2 = myCalc.divide(x,y);
+        System.out.println(result2);
+        myCalc.powerOff();
+    }
+}
