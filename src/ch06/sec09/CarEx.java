7c7e39c2bebe059553777b9570e672ea8d0771f3
Wooya <wooya_0@naver.com>
2024-04-16 18:16:27 +0900

    .

---
+++ src/ch06/sec09/CarEx.java
@@ -0,0 +1,11 @@
+package ch06.sec09;
+
+public class CarEx {
+    public static void main(String[] args) {
+        Car myCar = new Car("포르쉐");
+        Car yourCar = new Car("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
