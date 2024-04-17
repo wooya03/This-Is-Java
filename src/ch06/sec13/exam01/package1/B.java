43409730a4f87d171a75ec8c7f0c8000fd5ad39b
Wooya <wooya_0@naver.com>
2024-04-17 16:57:41 +0900

    .

---
+++ src/ch06/sec13/exam01/package1/B.java
@@ -0,0 +1,5 @@
+package ch06.sec13.exam01.package1; // 패키지가 동일
+
+public class B {
+    A a; // A 클래스 접근 가능(필드로 선언할 수 있음)
+}
---
+++ src/ch06/sec13/exam01/package2/C.java
@@ -0,0 +1,8 @@
+package ch06.sec13.exam01.package2; // 패키지가 다름
+
+import ch06.sec13.exam01.package1.*;
+
+public class C {
+    // A a; // A 클래스 접근 불가
+    B b;
+}
