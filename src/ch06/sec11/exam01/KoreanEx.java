23149cf0b321d7b3626305df9666a2d9b315290f
Wooya <wooya_0@naver.com>
2024-04-17 16:35:10 +0900

    .

---
+++ src/ch06/sec11/exam01/KoreanEx.java
@@ -0,0 +1,17 @@
+package ch06.sec11.exam01;
+
+public class KoreanEx {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("123456-1234567", "감자바");
+        System.out.println(k1.nation);
+        System.out.println(k1.ssn);
+        System.out.println(k1.name);
+
+        // final 필드는 값을 변경할 수 없음
+        // k1.nation = "USA";
+        // k1.ssn = "123-12-1234";
+
+        // 비 final 필드는 값 변경 가능
+        k1.name = "김자바";
+    }
+}
